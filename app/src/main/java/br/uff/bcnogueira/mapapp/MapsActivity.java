232b937ac293d086f6d404dec3a9d4b0d3fa37a2
bc-nogueira <bcnogueira@id.uff.br>
2018-09-25 12:08:01 -0300

    Commit inicial.

---
+++ app/src/main/java/br/uff/bcnogueira/mapapp/MapsActivity.java
@@ -0,0 +1,46 @@
+package br.uff.bcnogueira.mapapp;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in IC UFF and move the camera
+        LatLng icUff = new LatLng(-22.906187, -43.132947);
+        mMap.addMarker(new MarkerOptions().position(icUff).title("Marker in IC UFF"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(icUff, 15.0f));
+    }
+}
